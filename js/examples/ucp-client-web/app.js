5c9cc0b6b4332f34161f89f965ffb8e7a4638ce4
jetibest <jeti2121@hotmail.com>
2018-07-04 23:29:43 +0200

    Create app.js

---
+++ js/examples/ucp-client-web/app.js
@@ -0,0 +1,9 @@
+window.app = {
+    create: function(args)
+    {
+        var container = args.container;
+        var ucpclient = args.ucpclient;
+        
+        container.innerHTML = 'Hello world!';
+    }
+};
